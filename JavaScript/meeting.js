a75034087df8a7a2b76e815664b9273d65eb08ec
Jenny Moon <jenny0moon@gmail.com>
2023-03-21 10:42:46 +0000

    Add kata

---
+++ JavaScript/meeting.js
@@ -0,0 +1,39 @@
+//https://www.codewars.com/kata/59df2f8f08c6cec835000012/train/javascript
+
+// kata(6kyu):
+// John has invited some friends. His list is:
+
+// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+// Could you make a program that
+
+// makes this string uppercase
+// gives it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+// So the result of function meeting(s) will be:
+
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+// It can happen that in two distinct families with the same family name two people have the same first name too.
+
+
+// My solution:
+
+function meeting(s) {
+  const arr = s.toUpperCase().split(';');
+  const guests = arr.map((guest) => {
+    const [firstName, lastName] = guest.split(':');
+    return { firstName, lastName };
+  });
+
+  guests.sort(
+    (a, b) =>
+      a.lastName.localeCompare(b.lastName) ||
+      a.firstName.localeCompare(b.firstName)
+  );
+
+  const result = guests
+    .map((guest) => `(${guest.lastName}, ${guest.firstName})`)
+    .join('');
+    
+  return result;
+}
